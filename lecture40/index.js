02acc41f1930fa3b65414ec143fb2ecdc9903dcc
sanjaykazi <sanjaykazi1149@gmail.com>
2024-11-02 23:01:59 +0530

    for loop done

--- lecture40/index.js
+++ lecture40/index.js
@@ -1,1 +1,24 @@
 console.log("Hello world")
+console.log( 1 < 2 ? "sanjay" : 'kazi')
+// logical operator
+// logical operator with non-boolean
+// 1. falsey value -> nan, null, undefined
+// 2. truthy value -> anything that is not falsey
+// short-circuiting 
+console.log(false || true && true || 5)
+// working only on forst condition later are ignored and this 
+// behaviour is called short circuiting
+console.log("Loop starts...")
+for(let i = 1; i <=10; i++){
+    if(i == 4){
+        console.log("continueing the loop")
+        continue;
+    }
+    else if(i == 9){
+        console.log("Loop breaked...")
+        break;
+    }
+    else{
+        console.log(i)
+    }
+}
