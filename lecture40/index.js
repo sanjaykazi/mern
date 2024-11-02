c239a4a9dbfd6b53c97933a419256ae78d688d11
sanjaykazi <sanjaykazi1149@gmail.com>
2024-11-02 23:09:04 +0530

    while loop done

--- lecture40/index.js
+++ lecture40/index.js
@@ -8,17 +8,31 @@ console.log( 1 < 2 ? "sanjay" : 'kazi')
 console.log(false || true && true || 5)
 // working only on forst condition later are ignored and this 
 // behaviour is called short circuiting
-console.log("Loop starts...")
-for(let i = 1; i <=10; i++){
-    if(i == 4){
-        console.log("continueing the loop")
-        continue;
-    }
-    else if(i == 9){
-        console.log("Loop breaked...")
-        break;
-    }
-    else{
-        console.log(i)
-    }
-}
+// console.log("Loop starts...")
+// for(let i = 1; i <=10; i++){
+//     if(i == 4){
+//         console.log("continueing the loop")
+//         continue;
+//     }
+//     else if(i == 9){
+//         console.log("Loop breaked...")
+//         break;
+//     }
+//     else{
+//         console.log(i)
+//     }
+// }
+// let i = 1;
+// while(i<6){
+//     if(i == 3){
+//         i++;
+//         continue;
+//     }
+//     else if(i == 5){
+//         break;
+//     }
+//     else{
+//         console.log(i)
+//     }
+//     i++;
+// }
